2578b46c236028b58b2825b4291f29652a94b249
Nooranah Malberg <nooranahmalberg@nooranahs-mbp.home>
2018-12-15 18:21:52 +1000

    Updated gatsby to version 2 to fix security issues Followed migration guide https://www.gatsbyjs.org/docs/migrating-from-v1-to-v2/

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <div>
+  <Layout>
     <h1>Learn to <br />design and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
     <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  </Layout>
 )
 
 export default IndexPage
